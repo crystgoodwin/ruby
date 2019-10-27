c328e00b1b35496d3df42fb6c0dbe0e1cd69a0d7
Crystal Goodwin <sgcg03@mac.com>
2019-10-27 15:56:56 -0400

    Created new practice file case.rb

---
+++ ruby_challenges/case.rb
@@ -0,0 +1,14 @@
+weather = "sunny"
+
+case weather
+when "cloudy"
+    puts "It's cloudy outside, you don't need your sunglasses"
+when "foggy"
+    puts "It's foggy outside, grab a flashlight"
+when "rainy"
+    puts "It's rainy outside, you're going to need your umbrella"
+when "sunny"
+    puts "It's sunny outside, grab your sunglasses and a hat"
+else
+    puts "Hmm, I don't know what you need to wear!"
+end
