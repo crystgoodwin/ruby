baea67dacb5621954b67169b05c04c79cbfdbbe7
Crystal Goodwin <sgcg03@mac.com>
2019-10-27 15:47:39 -0400

    Created new practice file if_else.rb

---
+++ ruby_challenges/if_else.rb
@@ -0,0 +1,28 @@
+if 1 + 1 == 2
+    puts "1 and 1 does indeed equal 2" 
+end
+
+my_name = "Crystal"
+if my_name == "Skillcrush"
+    puts "Helloooo, Skillcrush"
+else
+    puts "Oops, I thought your name was Skillcrush. Sorry about that, #{my_name}!"
+end
+
+
+fav_color = "purple"
+if (fav_color == "purple")
+    puts "Purple like royalty!"
+elsif (fav_color == "orange")
+    puts "Orange like, well... an orange."
+elsif (fav_color == "yellow")
+    puts "Yellow daffodils are so pretty in the spring"
+elsif (fav_color == "green")
+    puts "Have you been to the Emerald City in Oz?"
+elsif (fav_color == "blue")
+    puts "Blue like the sky"
+elsif (fav_color == "red")
+    puts "Red like fire!"
+else
+    puts "Hmm, well I don't know what that color is!"
+end
