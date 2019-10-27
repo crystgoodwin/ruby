a6c1417dafb547cc3aa864cd02674d2cfa740ff6
Crystal Goodwin <sgcg03@mac.com>
2019-10-27 15:21:49 -0400

    Created new folder ruby_challenges

---
+++ ruby_challenges/always_three.rb
@@ -0,0 +1,10 @@
+puts "Give me a number."
+number_1 = gets.to_i
+mid_number = number_1 + 5
+mid_number = mid_number * 2
+mid_number = mid_number - 4
+mid_number = mid_number / 2
+final_number = mid_number - number_1
+puts "Always #{final_number}"
+
+
