a248c2a7b39dec3c3d40f57559e8d9d7df47e65e
Crystal Goodwin <sgcg03@mac.com>
2019-10-27 22:33:06 -0400

    Made changes to always three file

--- ruby_challenges/always_three.rb
+++ ruby_challenges/always_three.rb
@@ -1,10 +1,11 @@
 puts "Give me a number."
 number_1 = gets.to_i
-mid_number = number_1 + 5
-mid_number = mid_number * 2
-mid_number = mid_number - 4
-mid_number = mid_number / 2
-final_number = mid_number - number_1
+final_number = number_1
+final_number+=5
+final_number*=2
+final_number-=4
+final_number/=2
+final_number-=number_1
 puts "Always #{final_number}"
 
 
